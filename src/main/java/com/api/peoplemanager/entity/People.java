1230aceaeb6cbdeabccfb9fcfc28cc896ecf8163
DeividBorges93 <deividmborges@gmail.com>
2023-01-18 13:09:22 -0300

    feat(entity): :sparkles: Cria entidade "People"
    recebe um ID, um nome, data de nascimento e um id de endereço

---
+++ src/main/java/com/api/peoplemanager/entity/People.java
@@ -0,0 +1,32 @@
+package com.api.peoplemanager.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data // criar getter e setter e o toString
+@AllArgsConstructor // cria construtor com as propriedades de pessoa
+@NoArgsConstructor // cria construtor vazio
+@Builder // ajudar na criação de objetos pessoa
+@Entity // informar que é uma entidade do BD
+
+public class People implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birthDate", nullable = false)
+    private String birthDate;
+
+    @Column(name = "addressId", nullable = false)
+    private Number addressId;
+
+}
