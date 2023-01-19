15dc990fb941a83728eafc5df13237ca9e6d708f
DeividBorges93 <deividmborges@gmail.com>
2023-01-19 13:08:51 -0300

    refactor(repository): :art: adiciona Bean @Repository aos repositórios

--- src/main/java/com/api/peoplemanager/repository/AddressesRepository.java
+++ src/main/java/com/api/peoplemanager/repository/AddressesRepository.java
@@ -2,7 +2,9 @@ package com.api.peoplemanager.repository;
 
 import com.api.peoplemanager.entity.Addresses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressesRepository extends JpaRepository<Addresses, Long> {
 
 }
