e66f4ff856a5fa743b247e19ef5da34584973a01
DeividBorges93 <deividmborges@gmail.com>
2023-01-19 13:10:19 -0300

    refactor(api): :bug: adiciona @EnableJpaRepositories

--- src/main/java/com/api/peoplemanager/PeopleManagerApplication.java
+++ src/main/java/com/api/peoplemanager/PeopleManagerApplication.java
@@ -4,9 +4,15 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PeopleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagerApplication.class, args);
+	}
 
 	@Bean
 	public ModelMapper modelMapper() {
@@ -14,9 +20,4 @@ public class PeopleManagerApplication {
 		modelMapper.getConfiguration().setSkipNullEnabled(true);
 		return modelMapper;
 	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(PeopleManagerApplication.class, args);
-	}
-
 }
